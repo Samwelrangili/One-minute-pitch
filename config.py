0d9110517c24707261766b919d637bfd9b26b9fb
Samwelrangili <samwelrangili896@gmail.com>
2020-05-12 11:17:34 +0300

    updated the whole project

---
+++ config.py
@@ -0,0 +1,54 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://samuel:Sam0758597216@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATION = True
+
+    # email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    DEBUG = True
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+   
+    
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://samuel:Sam0758597216@localhost/pitch'
+
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://samuel:Sam0758597216@localhost/pitch'
+    
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
